1512c50341f2c1d3936ec69e9aeef960ee3b4940
kim-sung-jun <dd8873@naver.com>
2021-01-08 11:17:04 +0900

    문제풀이 추가~

--- src/beakjoon/Q11021.java
+++ src/beakjoon/Q11021.java
@@ -1,17 +1,23 @@
 package beakjoon;
 
 import java.util.Scanner;
+
 //2021.01.07
 //A+B - 7
 public class Q11021 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        int t = sc.nextInt();
+        StringBuilder stringBuilder = new StringBuilder();
 
-        for
-
-
+        for (int i = 1; i <= t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            String x = "Case #" + i + ": " + a + " + " + b + " = " + (a + b);
+            stringBuilder.append(x + "\n");
+        }
+        System.out.println(stringBuilder.toString());
     }
 }
---
+++ src/beakjoon/Q2438.java
@@ -0,0 +1,20 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+//2021.01.08
+//별 찍기-1
+public class Q2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
