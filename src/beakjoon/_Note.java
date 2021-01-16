8f3e28967b7f5f78d5f0b932223142772cffcecc
kim-sung-jun <dd8873@naver.com>
2021-01-16 16:50:12 +0900

    문제풀이 어려워.....

---
+++ src/beakjoon/_Note.java
@@ -0,0 +1,36 @@
+package beakjoon;
+//2021.01.16
+//연습장
+
+//
+//import java.util.Scanner;
+////charAt(); 사용 ex)
+//public class _Note {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        char c1 = ' ';
+//        char c2 = ' ';
+//
+//        c1 = sc.next().charAt(0);
+//        c2 = sc.nextLine().charAt(3);
+//
+//        System.out.println(c1);
+//        System.out.println(c2);
+//    }
+//}
+//
+//Integer.parseInt(); 사용 ex)
+//public class _Note {
+//    public static void main(String[] args) {
+//        String strNum1 = new String("1234");
+//        String strNum2 = new String("4321");
+//
+//        int num1 = Integer.parseInt(strNum1);
+//        int num2 = Integer.parseInt(strNum2);
+//
+//        System.out.println(num1 + num2);
+//    }
+//}
+//
+
