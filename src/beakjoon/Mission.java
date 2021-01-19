879b6a91b5e1ed89e04c729e799b91971c983083
kim-sung-jun <dd8873@naver.com>
2021-01-19 22:10:37 +0900

    풀어가는중! 생각중!

--- src/beakjoon/Mission.java
+++ src/beakjoon/Mission.java
@@ -3,7 +3,7 @@ package beakjoon;
 import java.util.Random;
 import java.util.Scanner;
 //2021.01.19
-//숫자야구 연습중
+//숫자야구 연습중!
 public class Mission {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
