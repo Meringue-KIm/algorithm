d1cb4086cb65c4257b01ce2792974512d1042720
kim-sung-jun <dd8873@naver.com>
2021-01-26 22:19:28 +0900

    코드업 문제풀이

---
+++ src/CodeUp/easy_9.java
@@ -0,0 +1,18 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class easy_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if (true) {
+            if (num % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+    }
+}
