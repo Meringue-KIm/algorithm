a61f2300863ed24985e4da34098ab6582c88c9a5
kim-sung-jun <dd8873@naver.com>
2021-01-29 18:12:12 +0900

    문제풀이중.. 다시 풀기!

---
+++ src/beakjoon/Q14918.java
@@ -0,0 +1,14 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+//2021.01.29
+public class Q14918 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
--- src/beakjoon/Q17496.java
+++ src/beakjoon/Q17496.java
@@ -1,18 +1,22 @@
 package beakjoon;
-
+//2021.01.29
 import java.util.Scanner;
 
 public class Q17496 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int T여름기간 = scanner.nextInt();
+        int N여름기간 = scanner.nextInt();
         int T자라나는기간 = scanner.nextInt();
         int C빈칸 = scanner.nextInt();
         int P가격 = scanner.nextInt();
+
         int 바구니 = 0;
+        int day = 1 + T자라나는기간;
 
-        for (int i = 1; i < T여름기간; i++) {
-
+        while (day <= N여름기간) {
+            바구니 += C빈칸;
+            day += T자라나는기간;
         }
+        System.out.println(바구니 * P가격);
     }
 }
