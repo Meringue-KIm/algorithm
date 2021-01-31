b6892b30b19b57e0d8d4433be770af198e61a9b4
kim-sung-jun <dd8873@naver.com>
2021-01-31 14:20:48 +0900

    쉬운 문제풀이중

---
+++ src/beakjoon/Q5337.java
@@ -0,0 +1,9 @@
+package beakjoon;
+//2021.01.31
+public class Q5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
---
+++ src/beakjoon/Q5338.java
@@ -0,0 +1,11 @@
+package beakjoon;
+//2021.01.31
+public class Q5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
