97ef6b79a698dedf1e5d95c8533968ae639c5686
kim-sung-jun <dd8873@naver.com>
2021-02-01 23:46:01 +0900

    스타워즈

---
+++ src/beakjoon/Q9653.java
@@ -0,0 +1,17 @@
+package beakjoon;
+//2021.02.01
+public class Q9653 {
+    public static void main(String[] args) {
+        System.out.print("    8888888888  888    88888\n");
+        System.out.print("   88     88   88 88   88  88\n");
+        System.out.print("    8888  88  88   88  88888\n");
+        System.out.print("       88 88 888888888 88   88\n");
+        System.out.print("88888888  88 88     88 88    888888\n");
+        System.out.println();
+        System.out.print("88  88  88   888    88888    888888\n");
+        System.out.print("88  88  88  88 88   88  88  88\n");
+        System.out.print("88 8888 88 88   88  88888    8888\n");
+        System.out.print(" 888  888 888888888 88  88      88\n");
+        System.out.print("  88  88  88     88 88   88888888\n");
+    }
+}
