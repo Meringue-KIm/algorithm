90d14d1706e99c0a1bf02d5b856028398cfebc5e
kim-sung-jun <dd8873@naver.com>
2021-02-04 11:02:53 +0900

    런타임 에러 찾는중! -에러 찾음(다르게 풀어봄)

--- src/beakjoon/Q10093.java
+++ src/beakjoon/Q10093.java
@@ -1,26 +1,35 @@
 package beakjoon;
 //2021.02.03
 //런타임 에러 찾는중
+//에러 찾음
 import java.util.Scanner;
 
 public class Q10093 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        int count = 0;
+        long num1 = scanner.nextLong();
+        long num2 = scanner.nextLong();
 
-        for (int i = 0; i < num2; i++) {
-            if (num1 < i) {
-                count++;
-            }
+        if (num1 == num2) {
+            System.out.println(0);
+        } else {
+            System.out.println(Math.abs(num1 - num2) - 1);
         }
-        System.out.println(count);
-        for (int i = 0; i < num2; i++) {
-            if (num1 < i) {
-                System.out.print(i + " ");
-            }
+
+        long start = 0;
+        long end = 0;
+
+        if (num1 > num2) {
+            start = num2;
+            end = num1;
+        } else {
+            start = num1;
+            end = num2;
+        }
+        for (long i = start + 1; i < end; i++) {
+            System.out.print(i + " ");
         }
     }
 }
+
