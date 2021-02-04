e427135170f211535f6c36fcde08d69afc95512a
kim-sung-jun <dd8873@naver.com>
2021-02-04 15:14:58 +0900

    Method

---
+++ src/beakjoon/zz.java
@@ -0,0 +1,22 @@
+package beakjoon;
+
+public class zz {
+    public static void main(String[] args) {
+
+        str1();
+    }
+
+    private static void str1() {
+        System.out.println('-');
+        System.out.println('a');
+        System.out.println('a');
+
+        System.out.println('-');
+        System.out.println('a');
+        System.out.println('a');
+
+        System.out.println('-');
+        System.out.println('a');
+        System.out.println('a');
+    }
+}
