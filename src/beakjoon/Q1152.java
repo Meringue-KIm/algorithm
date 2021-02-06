206719c54a60e1788cec8f4643da48a13e0d153e
kim-sung-jun <dd8873@naver.com>
2021-02-06 14:37:40 +0900

    문제풀풀

---
+++ src/beakjoon/Q1152.java
@@ -0,0 +1,16 @@
+package beakjoon;
+//2021.02.06
+import java.util.Scanner;
+
+public class Q1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().trim();
+
+        if (str.isEmpty()) {
+            System.out.println(0);
+        } else {
+            System.out.println(str.split(" ").length);
+        }
+    }
+}
