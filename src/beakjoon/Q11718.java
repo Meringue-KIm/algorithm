27ceca0081c8189a4b06c12d540eb0ae15074878
kim-sung-jun <dd8873@naver.com>
2021-02-07 15:47:39 +0900

    문제풀풀

---
+++ src/beakjoon/Q11718.java
@@ -0,0 +1,15 @@
+package beakjoon;
+//2021.02.07
+import java.util.Scanner;
+
+public class Q11718 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            System.out.println(str);
+        }
+        scanner.close();
+    }
+}
