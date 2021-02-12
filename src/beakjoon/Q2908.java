70b940446d9df4987344ce783ecd22545f9b3d8c
kim-sung-jun <dd8873@naver.com>
2021-02-12 16:28:01 +0900

    StringBuilder().append(A).reverse().toString() 공부중

--- src/beakjoon/Q2908.java
+++ src/beakjoon/Q2908.java
@@ -1,15 +1,23 @@
 package beakjoon;
 //2021.02.08
 //아직 안풀었음
+//StringBuilder().append(A).reverse().toString() 공부하기
 import java.util.Scanner;
 
 public class Q2908 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
 
-        System.out.println(num1 + num2);
-        System.out.println(1%10);
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        in.close();
+
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+        System.out.print(A > B ? A : B);
     }
 }
