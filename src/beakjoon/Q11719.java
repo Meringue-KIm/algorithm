77767eb00363452a78d58d6908f00c21b8fc22c7
kim-sung-jun <dd8873@naver.com>
2021-02-13 14:22:13 +0900

    hasNextLine 사용하기

---
+++ src/beakjoon/Q11719.java
@@ -0,0 +1,14 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Q11719 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            System.out.println(str);
+        }
+    }
+}
