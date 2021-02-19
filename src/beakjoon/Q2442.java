64751445fd89766a088b982486fa4109770be80a
kim-sung-jun <dd8873@naver.com>
2021-02-19 15:34:18 +0900

    별 찍기 - 5

---
+++ src/beakjoon/Q2442.java
@@ -0,0 +1,19 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Q2442 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++)
+                System.out.print(" ");
+            for (int j = 0; j < i * 2 - 1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
+
