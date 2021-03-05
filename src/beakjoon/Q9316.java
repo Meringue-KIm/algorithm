91f10aaf873ae7e5306d1f7ec2aa3006a3e5e029
kim-sung-jun <dd8873@naver.com>
2021-03-05 17:17:05 +0900

    쉬어가는 출력문제

---
+++ src/beakjoon/Q9316.java
@@ -0,0 +1,15 @@
+package beakjoon;
+//2021.03.05
+import java.util.Scanner;
+
+public class Q9316 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Hello World, Judge "+i+"!");
+        }
+    }
+}
