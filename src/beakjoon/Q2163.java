3b8b19cc04f776d65f953bfce216d0337a577680
kim-sung-jun <dd8873@naver.com>
2021-03-14 17:14:13 +0900

    문제풀이 - 초콜릿 자르기

---
+++ src/beakjoon/Q2163.java
@@ -0,0 +1,12 @@
+package beakjoon;
+//2021.03.14
+import java.util.Scanner;
+
+public class Q2163 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A * B - 1);
+    }
+}
