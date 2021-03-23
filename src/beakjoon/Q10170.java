dd9c597a20f56a92d6932ed740b16f2e6cab4193
kim-sung-jun <dd8873@naver.com>
2021-03-23 23:59:45 +0900

    문제풀이 - NFC West vs North (하는중)

---
+++ src/beakjoon/Q10170.java
@@ -0,0 +1,16 @@
+package beakjoon;
+
+public class Q10170 {
+    public static void main(String[] args) {
+        System.out.println("NFC West       W   L  T");
+        System.out.println("-----------------------");
+        System.out.println("Seattle        13  3  0\n");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
