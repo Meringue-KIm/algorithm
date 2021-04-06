edcf726e8bb52fa4d52bd93ec8dfd1e122a834d1
kim-sung-jun <dd8873@naver.com>
2021-04-07 00:00:11 +0900

    문제풀이 - 초6 수학 (풀이중)

---
+++ src/beakjoon/Q2702.java
@@ -0,0 +1,19 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Q2702 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            if (a / 2 == 0 && b / 2 == 0) {
+
+            }
+        }
+    }
+}
