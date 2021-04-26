ab2a5e30b6984053e731765c3d871b65b4b5b770
kim-sung-jun <dd8873@naver.com>
2021-04-26 23:22:16 +0900

    문제풀이 - 학점계산 (푸는중)

---
+++ src/beakjoon/Q2754.java
@@ -0,0 +1,20 @@
+package beakjoon;
+//2021.04.26
+import java.util.Scanner;
+
+public class Q2754 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        double num = 4.3;
+
+        for (int i = 0; i < 13; i++) {
+            if () {
+
+            }
+        }
+
+
+    }
+}
