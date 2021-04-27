bc7d5e9ce950f815eee3c066ce6fa7ac33ca4cd4
kim-sung-jun <dd8873@naver.com>
2021-04-28 01:20:54 +0900

    문제풀이 - 오타맨 고창영 (홍센세 도움 굿)

--- src/beakjoon/Q2711_1.java
+++ src/beakjoon/Q2711_1.java
@@ -1,10 +1,8 @@
 package beakjoon;
-//2021.04.24
-//틀림 동일한 값을 모두 빼버리는 현상
-//2711_1 로 다시풀기
+//2021.04.28
 import java.util.Scanner;
 
-public class Q2711 {
+public class Q2711_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,18 +10,16 @@ public class Q2711 {
 
         for (int i = 0; i < T; i++) {
             int num = scanner.nextInt();
-            String str = scanner.nextLine();
+            String str = scanner.next();
 
             for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(num);
+                char[] ch = new char[str.length()];
+                ch[j] = str.charAt(j);
 
-                char[] arr = new char[str.length()];
-                arr[j] = str.charAt(j);
-
-                if (arr[j] != c) {
-                    System.out.print(arr[j]);
-                } else {
+                if (j == num - 1) {
                     continue;
+                } else {
+                    System.out.print(ch[j]);
                 }
             }
             System.out.println();
