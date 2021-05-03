e7fd9cf02d06ce34745f1a70aac125f99c83d52f
kim-sung-jun <dd8873@naver.com>
2021-05-03 23:47:39 +0900

    문제풀이 - N번째 큰 수 (생각중)

---
+++ src/beakjoon/Q2693.java
@@ -0,0 +1,9 @@
+package beakjoon;
+//2021.05.03
+import java.util.Scanner;
+
+public class Q2693 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
