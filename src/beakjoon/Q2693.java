9b8f0f8ffe47ed768cd62760d04449376c7674ca
kim-sung-jun <dd8873@naver.com>
2021-05-04 23:29:47 +0900

    문제풀이 - N번째 큰 수 (쉬운 문제)

--- src/beakjoon/Q2693.java
+++ src/beakjoon/Q2693.java
@@ -1,9 +1,22 @@
 package beakjoon;
 //2021.05.03
+//2021.05.04
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q2693 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+        int[] arr = new int[10];
+
+        for (int i = 0; i < t; i++) {
+            for (int j = 0; j < 10; j++) {
+                arr[j] = scanner.nextInt();
+            }
+            Arrays.sort(arr);
+            System.out.println(arr[7]);
+        }
     }
 }
