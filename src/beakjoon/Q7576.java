fa7e7ac5481c8145141aa2bcec8a6f2f3a25ebdf
kim-sung-jun <dd8873@naver.com>
2021-05-06 23:54:26 +0900

    문제풀이 - 그릇 (풀이중)

---
+++ src/beakjoon/Q7576.java
@@ -0,0 +1,21 @@
+package beakjoon;
+//2021.05.06
+import java.util.Scanner;
+
+public class Q7576 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        int 정답 = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i == 0) {
+                정답 = 정답 + 10;
+            }
+
+            if (str.charAt())
+        }
+        System.out.println(정답);
+    }
+}
