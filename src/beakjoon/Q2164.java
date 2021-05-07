fc1f2b5c105acb22071f661364636c0256ef0751
kim-sung-jun <dd8873@naver.com>
2021-05-07 18:02:07 +0900

    문제풀이 - 카드2 (코테 예시 문제집)

---
+++ src/beakjoon/Q2164.java
@@ -0,0 +1,30 @@
+package beakjoon;
+//2021.05.07
+import java.util.Scanner;
+
+public class Q2164 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i - 1] = i;
+        }
+
+        int count = 0;
+        int 저장소 = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = 0;
+            저장소 = arr[i + 1];
+            for (int j = 1 + count; j < N; j++) {
+                arr[j] = arr[j + 1];
+            }
+            count++;
+            arr[N] = 저장소;
+        }
+        System.out.println(저장소);
+    }
+}
