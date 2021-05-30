1f582b9fd33aaa9a5f852a84806052a99bbe12c0
kim-sung-jun <dd8873@naver.com>
2021-05-30 20:03:16 +0900

    문제풀이 - 새로운 시작 (다시시작 기념 문제풀이)

---
+++ src/beakjoon/Q15962.java
@@ -0,0 +1,10 @@
+package beakjoon;
+//2021.05.30
+import java.util.Scanner;
+
+public class Q15962 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("파이팅!!");
+    }
+}
