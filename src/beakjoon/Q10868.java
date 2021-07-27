f7552806bfcd653722f19c25c17c1436691d222b
kim-sung-jun <dd8873@naver.com>
2021-07-27 17:10:38 +0900

    문제풀이 - 10868 (ArrayList 활용)

---
+++ src/beakjoon/Q10868.java
@@ -0,0 +1,35 @@
+package beakjoon;
+//2021.07.27
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q10868 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> arr = new ArrayList<>();
+
+        int num = scanner.nextInt();
+        int 범위갯수 = scanner.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            arr.add(scanner.nextInt());
+        }
+
+        int 최솟값 = 1000000000;
+
+        for (int i = 0; i < 범위갯수; i++) {
+         int A = scanner.nextInt();
+         int B = scanner.nextInt();
+
+            for (int j = A - 1; j <= B - 1; j++) {
+                if (최솟값 > arr.get(j)) {
+                    최솟값 = arr.get(j);
+                }
+            }
+            System.out.println(최솟값);
+            최솟값 = 1000000000;
+        }
+    }
+}
