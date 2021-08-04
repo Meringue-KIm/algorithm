927ae359a84e19f21be2da2b4ba845f303824073
kim-sung-jun <dd8873@naver.com>
2021-08-04 17:07:52 +0900

    문제풀이 - 별찍기 안직 못풀었음

---
+++ src/beakjoon/Q10994.java
@@ -0,0 +1,21 @@
+package beakjoon;
+//2021.08.04
+import java.util.Scanner;
+
+public class Q10994 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        if (num == 1) {
+            System.out.print("*");
+        }
+        int count = num - 1;
+
+        for (int i = 0; i < num + count; i++) {
+            for (int j = 0; j < ; j++) {
+                
+            }
+        }
+    }
+}
