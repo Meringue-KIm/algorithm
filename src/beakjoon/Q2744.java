5f9cbf04e67c6fb856259f2fbb884c7a37841f28
kim-sung-jun <dd8873@naver.com>
2021-08-14 15:07:33 +0900

    문제풀이 - 대소문자 바꾸기 (toLowerCase, toUpperCase)

--- src/beakjoon/Q2744.java
+++ src/beakjoon/Q2744.java
@@ -1,6 +1,7 @@
 package beakjoon;
 //2021.08.07
 //2021.08.13
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,26 +9,17 @@ import java.util.Scanner;
 public class Q2744 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List <Character> arr = new ArrayList<>();
-//        char ch = 'a'+2;
-//        System.out.println(ch);
-        String str = scanner.next();
+        List<Character> arr = new ArrayList<>();
 
-
+        String str = scanner.nextLine();
         for (int i = 0; i < str.length(); i++) {
             arr.add(str.charAt(i));
-            if (arr.get(i) > 91) { //대문자
-                for (int j = 65; j < 92; j++) {
-                    if (arr.get(i) == j) {
-                        char 대문자 = 32;
-                        char 저장공간 = 0;
-                       저장공간 = arr.get(i);
-                       arr.add(i, (char)(대문자 + 저장공간));
-                    }
-                }
-                System.out.println(arr.get(i));
+
+            if (Character.isUpperCase(arr.get(i))) {
+                System.out.print(String.valueOf(arr.get(i)).toLowerCase());
+            } else {
+                System.out.print(String.valueOf(arr.get(i)).toUpperCase());
             }
         }
-
     }
 }
