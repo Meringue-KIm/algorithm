c212fc938f9d18d98b7a3b246a4955280bfe935c
kim-sung-jun <dd8873@naver.com>
2021-08-15 17:39:53 +0900

    문제풀이 - 크로아티아 알파벳 (contains, replaceAll)

---
+++ src/beakjoon/Q2941.java
@@ -0,0 +1,20 @@
+package beakjoon;
+//2021.08.15
+import java.util.Scanner;
+
+public class Q2941 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().trim();
+
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        int result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (str.contains(arr[i])) {
+                str = str.replaceAll(arr[i], " ");
+            }
+        }
+        System.out.println(str.length());
+    }
+}
