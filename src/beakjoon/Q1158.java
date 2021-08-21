fc3395ebef498339b8020d7e3b598cde1404cf36
kim-sung-jun <dd8873@naver.com>
2021-08-21 15:51:26 +0900

    문제풀이 - 요세푸스 문제 (LinkedList 새로운 활용)

--- src/beakjoon/Q1158.java
+++ src/beakjoon/Q1158.java
@@ -1,33 +1,36 @@
 package beakjoon;
 //2021.08.20
 //LinkedList 활용 맞춤
+//2021.08.21
+//LinkedList 활용
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Q1158 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        LinkedList<Integer> List = new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
         int N = scanner.nextInt();
         int K = scanner.nextInt();
 
         for (int i = 1; i <= N; i++) {
-            List.add(i);
+            list.add(i);
         }
 
         System.out.print("<");
 
-        while (!List.isEmpty()) {
+        while (!list.isEmpty()) {
             for (int i = 0; i < K; i++) {
+                int a = list.remove();
                 if (i == K - 1) {
-                    int a = List.remove();
-                    if (List.size() == 0) {
+                    if (list.size() == 0) {
                         System.out.print(a);
                     } else {
                         System.out.print(a + ", ");
                     }
                 } else {
-                    List.add(List.remove());
+                    list.add(a);
                 }
             }
         }
