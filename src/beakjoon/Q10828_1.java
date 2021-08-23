4e0a0630c5207de3b828e3b19f102cade4b21964
kim-sung-jun <dd8873@naver.com>
2021-08-23 23:58:25 +0900

    문제풀이 - 스택 (stack 활용 시간초과) (StringBuilder 사용 시간초과 해결)

--- src/beakjoon/Q10828_1.java
+++ src/beakjoon/Q10828_1.java
@@ -8,22 +8,29 @@ public class Q10828_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
         int N = scanner.nextInt();
+
         for (int i = 0; i < N; i++) {
             String input = scanner.next();
             if (input.contains("push")) {
                 int input2 = scanner.nextInt();
                 stack.push(input2);
             } else if (input.equals("pop")) {
-                System.out.println(stack.isEmpty() ? -1 : stack.pop());
+                sb.append(stack.isEmpty() ? -1 : stack.pop());
+                sb.append("\n");
             } else if (input.equals("size")) {
-                System.out.println(stack.size());
+                sb.append(stack.size());
+                sb.append("\n");
             } else if (input.equals("empty")) {
-                System.out.println(stack.isEmpty() ? 1 : 0);
+                sb.append(stack.isEmpty() ? 1 : 0);
+                sb.append("\n");
             } else if (input.equals("top")) {
-                System.out.println(stack.isEmpty() ? -1 : stack.peek());
+                sb.append(stack.isEmpty() ? -1 : stack.peek());
+                sb.append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
