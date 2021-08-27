fc25dae6be0e100c53acef0eb2a07a493b192e71
kim-sung-jun <dd8873@naver.com>
2021-08-27 23:53:31 +0900

    문제풀이 - 배수와 약수

---
+++ src/beakjoon/Q5086.java
@@ -0,0 +1,28 @@
+package beakjoon;
+//2021.08.27
+
+import java.util.Scanner;
+
+public class Q5086 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+
+            if (num1 == 0 && num2 == 0) {
+                break;
+            }
+
+            if (num2 % num1 == 0) {
+                System.out.println("factor");
+            } else if (num1 % num2 == 0) {
+                System.out.println("multiple");
+            } else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
