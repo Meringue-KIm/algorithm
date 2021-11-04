e8c1c28bc86bf04896c290869c5a7e59f0b78fce
kim-sung-jun <dd8873@naver.com>
2021-11-04 13:01:58 +0900

    프로그래머스 문제풀이 - 핸드폰 번호 가리기(Lv1)

---
+++ src/programmers/Lv1_3.java
@@ -0,0 +1,16 @@
+package programmers;
+//핸드폰 번호 가리기
+public class Lv1_3 {
+    public static String solution(String phone_number) {
+        int num = phone_number.length() - 4;
+        StringBuilder answer = new StringBuilder();
+        for (int j = 0; j < num; j++) {
+            answer.append("*");
+        }
+        for (int i = num; i < phone_number.length(); i++) {
+            char ch = phone_number.charAt(i);
+            answer.append(String.valueOf(ch));
+        }
+        return answer.toString();
+    }
+}
