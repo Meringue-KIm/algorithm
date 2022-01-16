ae41da3d1161b04ae6dbf2e5c130003b704a8784
kimsungjun <dd8873@naver.com>
2022-01-16 22:17:51 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv1_9.java
@@ -0,0 +1,11 @@
+package programmers;
+//짝수와 홀수
+public class Lv1_9 {
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
