530e230b66a5eaa6a8867864569b276eb4b19540
kimsungjun <dd8873@naver.com>
2022-01-18 21:44:37 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv_10.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+//k번째 수
+public class Lv_10 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        System.out.println(Arrays.toString(solution(array, commands)));
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] arr = new int[commands.length];
+        for (int i = 0; i < arr.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            int a = commands[i][0];
+            int b = commands[i][1];
+            int c = commands[i][2];
+            for (int j = a; j <= b; j++) {
+                list.add(array[j - 1]);
+            }
+            Collections.sort(list);
+            arr[i] = list.get(c - 1);
+        }
+        return arr;
+    }
+}
