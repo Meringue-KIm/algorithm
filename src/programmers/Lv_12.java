8f9e4292170b28d025fbb1e7a5a3af07c39e3893
kimsungjun <dd8873@naver.com>
2022-01-20 14:47:28 +0900

    프로그래머스 문제풀이 (정규식에 대한 공부가 더 필요함)

---
+++ src/programmers/Lv_12.java
@@ -0,0 +1,29 @@
+package programmers;
+
+//신규 아이디 추천
+//정규식 공부하기
+public class Lv_12 {
+
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+
+        if (answer.equals("")) {
+            answer += "a";
+        }
+
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$", "");
+        }
+
+        if (answer.length() <= 2) {
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length() - 1);
+            }
+        }
+        return answer;
+    }
+}
