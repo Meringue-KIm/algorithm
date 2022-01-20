50e7aa61cf55a78294a5bfdd1913943b580dd106
kimsungjun <dd8873@naver.com>
2022-01-20 15:12:22 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv_13.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lv_13 {
+
+    public long solution(long n) {
+        List<Long> list = new ArrayList<>();
+        int count = 0;
+        while (n > 0) {
+            list.add(n % 10);
+            n /= 10;
+            count++;
+        }
+        Collections.sort(list);
+        Collections.reverse(list);
+        long value = 0;
+        for (long l : list) {
+            value += l * (long) Math.pow(10, count - 1);
+            count--;
+        }
+        return value;
+    }
+}
