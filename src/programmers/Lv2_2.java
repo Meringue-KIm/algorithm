220ab4e99727f4658589115bdff5e7c4c8515601
kimsungjun <dd8873@naver.com>
2022-01-22 17:55:09 +0900

    프로그래머스 문제풀이 - 짝지어 제거하기 (효율성 테스트에서 떨어짐..)

---
+++ src/programmers/Lv2_2.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+//짝지어 제거하기 (효율성 테스트에서 떨어짐..)
+public class Lv2_2 {
+
+    public static int solution(String s) {
+        List<Character> list = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            list.add(s.charAt(i));
+        }
+        int count = 1;
+
+        for (int i = 0; i < list.size(); i++) {
+            char ch = list.get(i);
+            if (count < list.size()) {
+                if (list.get(count) == ch) {
+                    list.remove(count);
+                    list.remove(i);
+                    count = 0;
+                    i = -1;
+                }
+            }
+            count++;
+        }
+
+        if (list.size() == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
