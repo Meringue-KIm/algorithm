a81cbf3da0571625eadd56fbe15439c6cddd1bbb
kimsungjun <dd8873@naver.com>
2022-01-26 15:35:41 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv1_23.java
@@ -0,0 +1,22 @@
+package programmers;
+//두 정수 사이의 합
+public class Lv1_23 {
+
+    public static long solution(int a, int b) {
+        long answer = 0;
+        if (a == b) {
+            return a;
+        } else if (a > b) {
+            for (int i = b; i <= a; i++) {
+                answer += i;
+            }
+            return answer;
+        } else {
+            for (int i = a; i <= b; i++) {
+                answer += i;
+            }
+            return answer;
+        }
+    }
+
+}
