c9216cc500f67c3c9256ba8a4136b425f6af3f80
kimsungjun <dd8873@naver.com>
2022-01-27 17:32:10 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv1_27.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.*;
+
+public class Lv1_27 {
+
+    public static List<Integer> solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        return list;
+    }
+
+}
