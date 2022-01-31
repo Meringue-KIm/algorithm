026d710088a286879aaa421a88cadb801514a8e2
kimsungjun <dd8873@naver.com>
2022-01-31 15:44:53 +0900

    백준 문제풀이 (틀림...)

---
+++ src/beakjoon/Q10989.java
@@ -0,0 +1,22 @@
+package beakjoon;
+//2022.01.31
+//수 정렬하기3
+import java.util.*;
+
+public class Q10989 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        StringBuffer stringBuffer = new StringBuffer();
+        for (Integer integer : arr) {
+            stringBuffer.append(integer).append("\n");
+        }
+        System.out.println(stringBuffer);
+    }
+}
+
