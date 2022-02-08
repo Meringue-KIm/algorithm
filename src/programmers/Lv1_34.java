a8d8497cfcea922d425d037df764ca4fac28b498
kimsungjun <dd8873@naver.com>
2022-02-08 17:56:37 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv1_34.java
@@ -0,0 +1,26 @@
+package programmers;
+//문자열 내 p와 y의 개수
+public class Lv1_34 {
+
+    public boolean solution(String s) {
+        char[] ch = s.toCharArray();
+        int p = 0;
+        int y = 0;
+        for (char c : ch) {
+            if (c == 80 || c == 112) {
+                p++;
+            }
+            if (c == 89 || c == 121) {
+                y++;
+            }
+        }
+        if (p == 0 && y == 0) {
+            return true;
+        } else if (p == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
