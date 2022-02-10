b1c169736430b3563a73d13c290509970c7f2f89
kimsungjun <dd8873@naver.com>
2022-02-10 17:55:40 +0900

    ..

---
+++ src/programmers/Lv1_35.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Lv1_35 {
+
+    public static void main(String[] args) {
+        String s = "vdsvdsa";
+        String[] strings = {"asdasd", "wfwf", "eded"};
+
+    }
+
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        return answer;
+    }
+
+}
