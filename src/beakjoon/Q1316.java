998875a5d60acdfe5e316ba1b7f9f3d2a8924545
kimsungjun <dd8873@naver.com>
2022-02-13 21:53:31 +0900

    백준 문제풀이

---
+++ src/beakjoon/Q1316.java
@@ -0,0 +1,35 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Q1316 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            String str = scanner.next();
+            boolean[] bolArr = new boolean[26];
+            boolean bol = true;
+            for (int j = 0; j < str.length(); j++) {
+                int index = str.charAt(j);
+                if (bolArr[index]) {
+                    if (str.charAt(j) != str.charAt(j - 1)) {
+                        bol = false;
+                        break;
+                    }
+                } else {
+                    bolArr[index] = true;
+                }
+            }
+            if (bol) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
