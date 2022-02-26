cbcf7559ea6e32e0f03faead0adc0a8d496c9bc0
kimsungjun <dd8873@naver.com>
2022-02-26 21:48:02 +0900

    백준 문제풀이

---
+++ src/beakjoon/Q2965_1.java
@@ -0,0 +1,20 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Q2965_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int ab = b - a - 1;
+        int bc = c - b - 1;
+
+        System.out.println(Math.max(ab, bc));
+    }
+
+}
+
