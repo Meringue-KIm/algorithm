e81cfd58f5aef02626a55525027137b7e7ad9afc
kimsungjun <dd8873@naver.com>
2022-02-28 23:15:19 +0900

    프로그래머스 문제풀이(다시풀기..)

--- src/programmers/Lv1_33.java
+++ src/programmers/Lv1_33.java
@@ -2,18 +2,17 @@ package programmers;
 
 import java.util.*;
 //신고 결과 받기
-//실패..
 public class Lv1_33 {
-//
-//    public static void main(String[] args) {
-//        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-//        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
-//        int k = 2;
-//        System.out.println(Arrays.toString(solution(id_list, report, k)));
-//    }
-//
-//    public static int[] solution(String[] id_list, String[] report, int k) {
-//        Set<String> set = new HashSet<>(Arrays.asList(report));
-//        return
-//    }
+
+    public static void main(String[] args) {
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
+        int k = 2;
+        System.out.println(Arrays.toString(solution(id_list, report, k)));
+    }
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        Set<String> set = new HashSet<>(Arrays.asList(report));
+        return
+    }
 }
