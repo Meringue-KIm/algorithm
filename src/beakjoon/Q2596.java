1f6d1db4532990a2c27238862988cdb9a378f44e
kimsungjun <dd8873@naver.com>
2022-03-03 23:47:00 +0900

    백준 문제풀이(수정)

--- src/beakjoon/Q2596.java
+++ src/beakjoon/Q2596.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.Scanner;
 //비밀편지
 public class Q2596 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
