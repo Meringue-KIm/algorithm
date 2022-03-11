0d543d6924bb1d04f4689fa9ef0431ea6d03da75
kimsungjun <dd8873@naver.com>
2022-03-11 19:02:09 +0900

    백준 문제풀이 수정

--- src/beakjoon/Q15552.java
+++ src/beakjoon/Q15552.java
@@ -1,6 +1,5 @@
 package beakjoon;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,11 +8,13 @@ import java.util.StringTokenizer;
 //2021.01.07
 //빠른 A+B-----------------------------------존나 어려워서 준성이가 풀어줌
 public class Q15552 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(sc.readLine());
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++){
+        int n = Integer.parseInt(sc.readLine());
+
+        for (int i = 0; i < n; i++) {
             String s = sc.readLine(); // 한 줄 입력받기
             StringTokenizer st = new StringTokenizer(s); // 공백으로 분리하기 위해 StringTokenizer 객체가 필요함
             // BufferReader는 공백분리할때 쟤가 꼭필요. Scanner는 X
@@ -27,4 +28,5 @@ public class Q15552 {
         }
         System.out.println(stringBuilder.toString());
     }
+
 }
