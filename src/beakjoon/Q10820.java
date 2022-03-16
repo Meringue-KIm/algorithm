3061dcea48b9533986e7d5bcc297dd0df7089849
kimsungjun <dd8873@naver.com>
2022-03-16 19:11:35 +0900

    백준 문제풀이

---
+++ src/beakjoon/Q10820.java
@@ -0,0 +1,36 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Q10820 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int 소문자 = 0;
+        int 대문자 = 0;
+        int 숫자 = 0;
+        int 공백 = 0;
+
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            for (int i = 0; i < str.length(); i++) {
+                char[] chArr = str.toCharArray();
+                if (chArr[i] >= 97 && chArr[i] <= 122) {
+                    소문자++;
+                } else if (chArr[i] >= 65 && chArr[i] <= 90) {
+                    대문자++;
+                } else if (chArr[i] >= 48 && chArr[i] <= 57) {
+                    숫자++;
+                } else {
+                    공백++;
+                }
+            }
+            System.out.println(소문자 + " " + 대문자 + " " + 숫자 + " " + 공백);
+            소문자 = 0;
+            대문자 = 0;
+            숫자 = 0;
+            공백 = 0;
+        }
+    }
+
+}
