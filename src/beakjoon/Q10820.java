87f8cbf32859e58cee22834c3bdce74edbc5bee0
kimsungjun <dd8873@naver.com>
2022-03-17 23:14:37 +0900

    백준 문제풀이

--- src/beakjoon/Q10820.java
+++ src/beakjoon/Q10820.java
@@ -6,13 +6,13 @@ public class Q10820 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int 소문자 = 0;
-        int 대문자 = 0;
-        int 숫자 = 0;
-        int 공백 = 0;
 
         while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
+            int 소문자 = 0;
+            int 대문자 = 0;
+            int 숫자 = 0;
+            int 공백 = 0;
             for (int i = 0; i < str.length(); i++) {
                 char[] chArr = str.toCharArray();
                 if (chArr[i] >= 97 && chArr[i] <= 122) {
@@ -26,10 +26,6 @@ public class Q10820 {
                 }
             }
             System.out.println(소문자 + " " + 대문자 + " " + 숫자 + " " + 공백);
-            소문자 = 0;
-            대문자 = 0;
-            숫자 = 0;
-            공백 = 0;
         }
     }
 
