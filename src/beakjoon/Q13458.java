1f04ea7316651828d3a193d6d1701f99ac6b0b4a
kimsungjun <dd8873@naver.com>
2022-03-22 23:30:51 +0900

    백준 문제풀이(푸는중)

---
+++ src/beakjoon/Q13458.java
@@ -0,0 +1,23 @@
+package beakjoon;
+
+import java.util.Scanner;
+//시험 감독 (푸는중)
+public class Q13458 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+
+
+
+    }
+}
