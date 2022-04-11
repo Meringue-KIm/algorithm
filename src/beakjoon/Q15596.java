21fdf7ce3751fc4e7b7641eb276ec656a007443d
sungjun kim <dd8873@naver.com>
2022-04-11 23:38:57 +0900

    Test

--- src/beakjoon/Q15596.java
+++ src/beakjoon/Q15596.java
@@ -4,7 +4,7 @@ package beakjoon;
 //
 public class Q15596 {
     public static void main(String[] args) {
-        Test test = new Test();
+        CodeTest test = new CodeTest();
         test.sum();
         // 근데 백준에서는 이렇게 실행ㅎ시켜서 정답인지 확인하는게 아니라
         // 그냥 메서드만 잘 만들엇는지 테스트하는거임.
