82888140469c79ee5292e85a8aa39fe3f78d9082
sungjun kim <dd8873@naver.com>
2022-05-03 22:11:55 +0900

    t

--- src/beakjoon/JS_2.java
+++ src/beakjoon/JS_2.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class JS_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int count = 1;
-
         while (scanner.hasNextInt()) {
             int num = scanner.nextInt();
             int[] arr = new int[count];
