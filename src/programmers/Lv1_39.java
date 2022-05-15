702b6479df03edefd5db037559bf992659165713
sungjun kim <dd8873@naver.com>
2022-05-15 16:23:53 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv1_39.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.Arrays;
+//문자열 내 마음대로 정렬하기
+public class Lv1_39 {
+
+    public static String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            char ch = strings[i].charAt(n);
+            strings[i] = ch + strings[i];
+        }
+        Arrays.sort(strings);
+        for (int i = 0; i < strings.length; i++) {
+            answer[i] = "";
+            for (int j = 1; j < strings[i].length(); j++) {
+                answer[i] += strings[i].charAt(j);
+            }
+        }
+        return answer;
+    }
+
+}
