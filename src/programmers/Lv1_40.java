ac2a4da13b571e28d3cef6b5d3a95f39fbc925aa
sungjun kim <dd8873@naver.com>
2022-05-23 16:28:34 +0900

    프로그래머스 문제풀이

---
+++ src/programmers/Lv1_40.java
@@ -0,0 +1,31 @@
+package programmers;
+//최소직사각형
+public class Lv1_40 {
+
+    public static int solution(int[][] sizes) {
+        int length = sizes[0][0];
+        int width = sizes[0][1];
+        int index = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            for (int j = 0; j < sizes[i].length; j++) {
+                if (sizes[i][0] > length) {
+                    length = sizes[i][0];
+                }
+                if (sizes[i][1] > width) {
+                    width = sizes[i][1];
+                    index = i;
+                }
+            }
+            if ( sizes[index][1] > sizes[index][0]) {
+                int num = sizes[index][0];
+                sizes[index][0] = sizes[index][1];
+                sizes[index][1] = num;
+                length = sizes[0][0];
+                width = sizes[0][1];
+                i = -1;
+            }
+        }
+        return length * width;
+    }
+
+}
