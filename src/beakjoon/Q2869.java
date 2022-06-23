dc75af4f703d319500457525e1d290076071f72a
sungjun kim <dd8873@naver.com>
2022-06-23 22:13:04 +0900

    백준 문제풀이

--- src/beakjoon/Q2869.java
+++ src/beakjoon/Q2869.java
@@ -1,4 +1,22 @@
 package beakjoon;
 
+import java.util.Scanner;
+
 public class Q2869 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int V = scanner.nextInt();
+        int count = 1;
+        int max = 0;
+        while (max <= V) {
+            max += ((A * count) - (B * count));
+            count++;
+        }
+
+        System.out.println(count - 1);
+    }
+
 }
--- src/beakjoon/Q9012.java
+++ src/beakjoon/Q9012.java
@@ -7,22 +7,16 @@ import java.util.Stack;
 public class Q9012 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         int T = in.nextInt();
-
         for (int i = 0; i < T; i++) {
             System.out.println(solve(in.next()));    // nextLine()쓰면 안된다.
         }
     }
 
     public static String solve(String s) {
-
         Stack<Character> stack = new Stack<>();
-
         for (int i = 0; i < s.length(); i++) {
-
             char c = s.charAt(i);
-
             // 여는 괄호일 경우 스택에 넣는다.
             if (c == '(') {
                 stack.push(c);
