1474602200cd3166d197a1dd6ea8ee1f35b0675b
sungjun kim <dd8873@naver.com>
2022-07-05 22:50:50 +0900

    프로그래머스 문제풀이(dp)

--- src/programmers/Lv2_8.java
+++ src/programmers/Lv2_8.java
@@ -1,53 +1,18 @@
 package programmers;
 
-import java.util.LinkedList;
-import java.util.Queue;
 //멀리뛰기 실패
+//(dp공부)
 public class Lv2_8 {
 
-    public static void main(String[] args) {
-        System.out.println(solution(6));
-    }
+    public long solution(int n) {
+        if(n == 1) return 1;
 
-    public static long solution(int n) {
-        Queue<String> queue = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
-        int sum = 0;
-        while (sum != n) {
-            queue.add("1");
-            sb.append("1");
-            if (sum < n) {
-                queue.add("1");
-                sb.append("1");
-                sum += 1;
-            }
-            if (sum + 2 <= n) {
-                queue.add("2");
-                sb.append("2");
-                sum += 2;
-            }
-        }
-        int count = 1;
-        if (n % 2 == 0) {
-            count = 2;
-        }
-        System.out.println(sb);
+        long[] dp = new long[n];
+        dp[0] = 1;
+        dp[1] = 2;
 
-        StringBuilder s = new StringBuilder();
-        while (true) {
-            String str = queue.remove();
-            queue.add(str);
-            for (String value : queue) {
-                s.append(value);
-            }
-            System.out.println(s);
-            count++;
-            if (String.valueOf(sb).equals(s.toString())) {
-                break;
-            }
-            s = new StringBuilder();
-        }
-
-        return count % 1234567;
+        for(int i=2; i<n; i++)
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        return dp[n-1];
     }
 }
