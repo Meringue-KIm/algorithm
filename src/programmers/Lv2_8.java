023f33e44cfc09cc8a16345ddf0a7fe0444ae3be
sungjun kim <dd8873@naver.com>
2022-07-06 19:04:59 +0900

    프로그래머스 문제풀이(dp)

--- src/programmers/Lv2_8.java
+++ src/programmers/Lv2_8.java
@@ -15,4 +15,16 @@ public class Lv2_8 {
             dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
         return dp[n-1];
     }
+    //다른 코드 답
+    public int jumpCase(int num) {
+        int answer = 0;
+        int a = 0;
+        int b = 1;
+        for(int i =0; i < num; i++){
+            answer = a+b;
+            a = b;
+            b = answer;
+        }
+        return answer;
+    }
 }
