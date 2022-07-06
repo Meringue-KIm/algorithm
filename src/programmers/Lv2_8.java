d192f0d6e77da0cbb29ca492f0c923a9c0e5ccee
sungjun kim <dd8873@naver.com>
2022-07-06 19:10:16 +0900

    프로그래머스 문제풀이(dp) 수정

--- src/programmers/Lv2_8.java
+++ src/programmers/Lv2_8.java
@@ -5,26 +5,29 @@ package programmers;
 public class Lv2_8 {
 
     public long solution(int n) {
-        if(n == 1) return 1;
-
+        if (n == 1) {
+            return 1;
+        }
         long[] dp = new long[n];
         dp[0] = 1;
         dp[1] = 2;
-
-        for(int i=2; i<n; i++)
-            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
-        return dp[n-1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+        }
+        return dp[n - 1];
     }
+
     //다른 코드 답
     public int jumpCase(int num) {
         int answer = 0;
         int a = 0;
         int b = 1;
-        for(int i =0; i < num; i++){
-            answer = a+b;
+        for (int i = 0; i < num; i++) {
+            answer = a + b;
             a = b;
             b = answer;
         }
         return answer;
     }
+
 }
