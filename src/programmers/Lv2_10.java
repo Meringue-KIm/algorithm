7f8680143142f61a507644b33f8185997197027a
sungjun kim <dd8873@naver.com>
2022-07-11 19:20:53 +0900

    프로그래머스 문제풀이(아직 정답 결과모름)

---
+++ src/programmers/Lv2_10.java
@@ -0,0 +1,25 @@
+package programmers;
+//22.07.11
+//숫자의 표현
+public class Lv2_10 {
+
+    public static int solution(int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                if (sum > n) {
+                    break;
+                } else if (sum == n) {
+                    System.out.println(sum);
+                    count++;
+                    break;
+                } else {
+                    sum += j + i;
+                }
+            }
+        }
+        return count;
+    }
+
+}
