96e424c335bd248df71e10cb0edd8cfc4667f569
sungjun kim <dd8873@naver.com>
2022-08-04 19:01:18 +0900

    프로그래머스 문제풀이

--- src/programmers/Lv1_1.java
+++ src/programmers/Lv1_1.java
@@ -2,6 +2,7 @@ package programmers;
 
 //x만큼 간격이 있는 n개의 숫자
 public class Lv1_1 {
+
     public long[] solution(long x, int n) {
         long[] answer = new long[n];
         for (int i = 1; i <= answer.length; i++) {
@@ -10,4 +11,5 @@ public class Lv1_1 {
         }
         return answer;
     }
+
 }
