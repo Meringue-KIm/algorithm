4e14caf1b022f99c5827ee0f781502f8c4d5cd4a
sungjun kim <dd8873@naver.com>
2022-12-21 17:28:21 +0900

    test

--- src/beakjoon/Test0317.java
+++ src/beakjoon/Test0317.java
@@ -1,7 +1,13 @@
 package beakjoon;
 
+import java.util.*;
+
 public class Test0317 {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
 
     }
+
 }
