784a6f92cb42658f2638348297bf798b3f6ebdb3
Meringue-KIm <dd8873@naver.com>
2023-06-03 21:15:49 +0900

    문제풀이

--- src/beakjoon/Q2750.java
+++ src/beakjoon/Q2750.java
@@ -1,11 +1,10 @@
 package beakjoon;
 
-import javafx.scene.transform.Scale;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q2750 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
@@ -15,8 +14,9 @@ public class Q2750 {
             arr[i] = scanner.nextInt();
         }
         Arrays.sort(arr);
-        for (int i = 0; i <arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int j : arr) {
+            System.out.println(j);
         }
     }
+
 }
