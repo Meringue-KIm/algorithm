a145033e5c5b4252b164bb55a419fc940bee87b9
Meringue-KIm <dd8873@naver.com>
2024-11-18 19:11:03 +0900

    프로그래머스: [PCCE 기출문제] 1번 / 문자 출력

---
+++ src/programmers/Lv0_7.java
@@ -0,0 +1,13 @@
+package programmers;
+// [PCCE 기출문제] 1번 / 문자 출력
+public class Lv0_7 {
+
+    public static void main(String[] args) {
+        String s = "good";
+
+        System.out.println("3\n2\n1");
+        System.out.println(s);
+
+    }
+
+}
